a75f1df00c24ec805b62aa642df9e948ba97a0d2
Yonghu Liu <s313442@studenti.polito.it>
2025-03-14 12:59:11 +0100

    voxel update

---
+++ converter.py
@@ -0,0 +1,133 @@
+from torch.utils.data import Dataset
+import pickle as pkl
+import os.path
+import torch
+import numpy as np
+import kaolin as kal
+
+affordance_descriptions = {
+            "grasp": "a highlighted handle for grasping",
+            "contain": "a highlighted space for containing objects",
+            "lift": "a highlighted section for lifting",
+            "openable": "a highlighted part that can be opened",
+            "layable": "a surface for laying items",
+            "sittable": "a seat for sitting",
+            "support": "a structure for support",
+            "wrap_grasp": "an area that can be wrap-grasped",
+            "pourable": "a spout for pouring",
+            "move": "a part that enables movement",
+            "display": "a surface for displaying objects",
+            "pushable": "a highlighted panel for pushing",
+            "pull": "a handle for pulling",
+            "listen": "a part for listening",
+            "wear": "a wearable section",
+            "press": "a button for pressing",
+            "cut": "a sharp edge for cutting",
+            "stab": "a pointed section for stabbing"
+        }
+
+class AffordNetDataset(Dataset):
+    def __init__(self, data_dir, data):
+        super().__init__()
+        self.data_dir = data_dir
+        self.data = data
+
+    def load_data(self):
+        self.all_data = []
+
+        with open(os.path.join(self.data_dir, self.data), 'rb') as f:
+            temp_data = pkl.load(f)
+
+        if isinstance(temp_data, dict):
+            n_arr = list(temp_data.keys())
+        elif isinstance(temp_data, list):
+            n_arr = list(temp_data[0].keys())
+
+        for index, info in enumerate(temp_data):
+            temp_info = {}
+            temp_info["shape_id"] = info[n_arr[0]]
+            temp_info["semantic_class"] = info[n_arr[1]]
+            temp_info["affordance"] = info[n_arr[2]]
+            temp_info["data_info"] = info[n_arr[3]]
+
+            #tem_char = info[n_arr[3]].keys()
+            temp_info["coordinates"] = info[n_arr[3]]['coordinate']
+            temp_info["labels"] = filter_non_zero_entries(info[n_arr[3]]['label'])
+            self.all_data.append(temp_info)
+
+        return self.all_data
+
+def filter_non_zero_entries(input):
+    # Create a new dictionary to store the filtered entries
+    filtered_labels = []
+    # Iterate over each item in the input dictionary
+    for key, value in input.items():
+        # Check if the numpy array is not all zeros
+
+        #return a dict(label:values)
+        # if np.any(value):
+        #     filtered_dict = {}
+        #     filtered_dict[key] = value
+        #     filtered_labels.append(filtered_dict)
+
+        # return a list(label)
+        if np.any(value):
+            filtered_label = key
+            filtered_labels.append(filtered_label)
+    return filtered_labels
+
+def generate_clip_sentences(semantic_class, labels):
+    # Generate a list of descriptions for each affordance
+    if semantic_class:
+        clip_texts = []
+        for label in labels:
+            clip_text = f"A 3D render of {semantic_class.lower()} with {affordance_descriptions.get(label, f'a highlighted {label}')}"
+            clip_texts.append(clip_text)
+
+    return clip_texts
+
+
+
+def point_to_voxel(coordinate, resolution=3):
+    if coordinate is not None:
+        coordinate = (coordinate - coordinate.min()) / (coordinate.max() - coordinate.min())
+        voxel_object = kal.ops.conversions.pointclouds_to_voxelgrids(pointclouds=coordinate, resolution=resolution)
+    else:
+        print("the coordinate is not useful")
+    return voxel_object
+
+
+def voxel_to_meshs(voxtel_object):
+    vertices, faces = kal.ops.conversions.voxelgrids_to_trianglemeshes(voxtel_object)
+
+    vertices = vertices[0].squeeze(0)  # (34, 3)
+    faces = faces[0].squeeze(0)  # (64, 3)
+
+    face_vertices = vertices[faces]  # (64, 3, 3)
+    face_vertices = face_vertices.unsqueeze(0)
+
+    face_normals = kal.ops.mesh.face_normals(face_vertices, unit=True)
+
+    face_normals = face_normals.unsqueeze(3).repeat(1, 1, 1, 3)
+    print(f"faces shape: {faces.shape}")
+    print(f"face_normals shape: {face_normals.shape}")
+
+    vertex_normals = kal.ops.mesh.compute_vertex_normals(faces, face_normals, len(vertices))
+
+    vertex_normals = vertex_normals[0].squeeze(0)
+
+    return vertices, faces, vertex_normals
+
+
+def save_obj(filepath, vertices, vertex_normals, faces):
+
+    with open(filepath, 'w') as f:
+        for v in vertices:
+            f.write(f"v {v[0]} {v[1]} {v[2]}\n")
+        for vn in vertex_normals:
+            f.write(f"vn {vn[0]} {vn[1]} {vn[2]}\n")
+        for face in faces:
+            f.write(f"f {face[0]+1}//{face[0]+1} {face[1]+1}//{face[1]+1} {face[2]+1}//{face[2]+1}\n")
+
+
+
---
+++ voxel_mesh.py
@@ -0,0 +1,120 @@
+import pickle
+import os
+import math
+
+import numpy as np
+import torch
+import trimesh
+from utils import device
+from pathlib import Path
+import random
+from AffoLoad import PklLoader
+import converter
+from converter import AffordNetDataset
+from converter import generate_clip_sentences, point_to_voxel, voxel_to_meshs, save_obj
+
+
+def create_voxel_from_mesh(args):
+    data_dir = 'AffoNet'
+    data_name = 'full_shape_val_data.pkl'
+
+    affordnet = AffordNetDataset(data_dir, data_name)
+    data = affordnet.load_data()
+    if not data:
+        raise ValueError("Loaded data is empty. Please check dataset path and contents.")
+    print(data[0].keys())
+
+    save_path = (os.path.join(data_dir, 'data_from_voxel'))
+    Path(save_path).mkdir(parents=True, exist_ok=True)
+
+    random.seed(args.seed)
+    rand_index = random.randint(0, len(data) - 1)
+    single_object = data[rand_index]
+
+    if 'semantic_class' not in single_object or 'labels' not in single_object or 'coordinates' not in single_object:
+        raise KeyError("Missing required keys in data entry.")
+
+    clip_texts = generate_clip_sentences(single_object['semantic_class'], single_object['labels'])
+
+    coordinates = torch.tensor(single_object['coordinates']).unsqueeze(0)
+    print("Coordinates Shape:", coordinates.shape)
+    print("Coordinates Device:", coordinates.device)
+
+    voxel = point_to_voxel(coordinates).to(device)
+
+    vertices, faces, vertex_normals = voxel_to_meshs(voxel)
+
+    # vertices = vertices[0].squeeze(0)  # shape to (34, 3)
+    vertices_ = vertices.cpu().numpy()
+    # faces = faces[0].squeeze(0)  # shape to  (64, 3)
+    faces_ = faces.cpu().numpy()
+
+    vertex_normals_ = vertex_normals.cpu().numpy()
+
+    # mesh = trimesh.Trimesh(vertices=vertices_, faces=faces_)
+
+    obj_file_path = os.path.join(save_path, f"{single_object['semantic_class']}.obj")
+    # mesh.export(obj_file_path)
+
+    save_obj(obj_file_path, vertices_, vertex_normals_, faces_)
+
+    print(f"Mesh saved at: {obj_file_path}")
+
+    args.object = single_object['semantic_class']
+    labels = single_object['labels']
+
+    # args.classes = single_object['labels']
+    # args.prompt = clip_texts
+
+    return args, clip_texts, obj_file_path, labels
+
+
+def voxel_from_mesh(args, Affloader):
+    data_dir = 'AffoNet'
+    save_path = (os.path.join(data_dir, 'data_from_voxel'))
+    Path(save_path).mkdir(parents=True, exist_ok=True)
+
+    shape_ids = Affloader.get_shape_id()
+    coordinates = Affloader.get_coordinates()
+    labels = Affloader.get_label()
+    semantic_labels = Affloader.get_semantic_class()
+    Affloader.generate_clip_sentences()
+    clip_sentences = Affloader.get_clip_sentences()
+
+    random.seed(args.seed)
+    rnumber = 0
+    # rnumber = random.randint(0, len(shape_ids) - 1)
+    shape_id = shape_ids[rnumber]
+    coordinates = coordinates[rnumber]
+    coordinates = torch.tensor(coordinates).unsqueeze(0)
+    print("Coordinates Shape:", coordinates.shape)
+    #print("Coordinates Device:", coordinates.device)
+
+    label = converter.filter_non_zero_entries(labels[rnumber])
+    clip_sentence = clip_sentences[rnumber]
+    clip_texts = clip_sentence
+    semantic_label = semantic_labels[rnumber]
+
+    voxel = point_to_voxel(coordinates).to(device)
+    vertices, faces, vertex_normals = voxel_to_meshs(voxel)
+
+    # vertices = vertices[0].squeeze(0)  # shape to (34, 3)
+    vertices_ = vertices.cpu().numpy()
+    # faces = faces[0].squeeze(0)  # shape to  (64, 3)
+    faces_ = faces.cpu().numpy()
+
+    vertex_normals_ = vertex_normals.cpu().numpy()
+
+    # mesh = trimesh.Trimesh(vertices=vertices_, faces=faces_)
+
+    obj_file_path = os.path.join(save_path, f"{semantic_label}.obj")
+    # mesh.export(obj_file_path)
+
+    save_obj(obj_file_path, vertices_, vertex_normals_, faces_)
+
+    print(f"Mesh saved at: {obj_file_path}")
+
+    args.object = semantic_label
+    labels = label
+
+    return args, clip_texts, obj_file_path, labels
